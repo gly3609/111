1be148336b727f0bf6cf0dd98d5ee4282f8aca08
gly3609 <91117432+gly3609@users.noreply.github.com>
2023-12-20 01:31:27 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -216,10 +216,6 @@ export function SideBar(props: { className?: string }) {
               <IconButton icon={<SettingsIcon />} shadow />
             </Link>
           </div>
-          <div className={styles["sidebar-action"]}>
-              <IconButton icon={<GithubIcon />} shadow />
-            </a>
-          </div>
         </div>
         <div>
           <IconButton
